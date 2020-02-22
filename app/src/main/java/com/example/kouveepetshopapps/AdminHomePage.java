31203127a451c53bc065cdb508ab1dda8c14a697
pradnyadarsana <pradnyadarsana13@gmail.com>
2020-02-22 23:45:37 +0700

    create basic home UI

--- app/src/main/java/com/example/kouveepetshopapps/AdminHomePage.java
+++ app/src/main/java/com/example/kouveepetshopapps/AdminHomePage.java
@@ -3,14 +3,12 @@ package com.example.kouveepetshopapps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class AdminHomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_admin_home_page);
     }
 }
